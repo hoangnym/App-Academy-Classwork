92c9397a4a5de2ba02192cec03f45d9f86ef319a
hoangnym <your_email_here>
2021-05-04 00:20:26 +0200

    Add fibonacci function

--- W2D1/javascript_ex/phase_4_recursion.js
+++ W2D1/javascript_ex/phase_4_recursion.js
@@ -25,20 +25,16 @@ function sumRec(arr)  {
 
 // console.log(sumRec(range(1, 10)));
 
-// [1] => 1
-// [1, 2] => 1 + [2]
-// [1, 2, 3] => 1 + [2, 3] => 1 + 2 + [3]
-
 
 function exponent1(base, exp) {
+    // exp(b, 0) = 1
+    // exp(b, n) = b * exp(b, n - 1)
+
     if (exp == 0) {return 1};
     
     return base * exponent1(base, exp - 1);
 }
 
-// # version 1
-// exp(b, 0) = 1
-// exp(b, n) = b * exp(b, n - 1)
 
 // console.log(exponent1(1, 0));
 // console.log(exponent1(2, 1));
@@ -46,6 +42,10 @@ function exponent1(base, exp) {
 // console.log(exponent1(3, 4));
 
 function exponent2(base, exp) {
+    // exp(b, 0) = 1
+    // exp(b, 1) = b
+    // exp(b, n) = exp(b, n / 2) ** 2             [for even n]
+    // exp(b, n) = b * (exp(b, (n - 1) / 2) ** 2) [for odd n]
     if (exp == 0) {return 1};
     if (exp == 1) {return base};
 
@@ -56,13 +56,41 @@ function exponent2(base, exp) {
     }
 }
 
-console.log(exponent2(1, 0));
-console.log(exponent2(2, 1));
-console.log(exponent2(3, 2));
-console.log(exponent2(3, 4));
+// console.log(exponent2(1, 0));
+// console.log(exponent2(2, 1));
+// console.log(exponent2(3, 2));
+// console.log(exponent2(3, 4));
 
-// # version 2
-// exp(b, 0) = 1
-// exp(b, 1) = b
-// exp(b, n) = exp(b, n / 2) ** 2             [for even n]
-// exp(b, n) = b * (exp(b, (n - 1) / 2) ** 2) [for odd n]
+
+
+function fibonacci(n) {
+    // fibonacci(n) - receives an integer, n, and returns the first n Fibonacci numbers
+    if (n == 0) { return 0 };
+    if (n == 1) { return 1 };
+
+    return fibonacci(n-1) + fibonacci(n-2)
+
+}
+
+// 2 ==> [0, 1, 1] => [0, 1] << [0] + [1]
+
+// console.log(fibonacci(1));
+// console.log(fibonacci(0));
+// console.log(fibonacci(2));
+// console.log(fibonacci(3));
+
+function fib_seq(n) {
+    let arr = []
+    for (let i = 0; i <= n; i++) {
+        arr.push(fibonacci(i));
+    }
+    return arr
+}
+
+
+console.log(fib_seq(1));
+console.log(fib_seq(0));
+console.log(fib_seq(2));
+console.log(fib_seq(5));
+
+
